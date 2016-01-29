f5c951ca3850562f72478e41806a5cb46643f2c2
smalten-kx <cherif.ismael@gmail.com>
2016-01-29 13:25:15 +0100

    Initial commit

---
+++ src/main/java/base/Personne.java
@@ -0,0 +1,42 @@
+package base;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by KX on 29/01/2016.
+ */
+public class Personne {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int annee_de_naissance) {
+
+        this.age = new DateTime().getYear() - annee_de_naissance;
+    }
+}
---
+++ src/test/java/base/PersonneTest.java
@@ -0,0 +1,19 @@
+package base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by KX on 29/01/2016.
+ */
+public class PersonneTest {
+
+    private Personne personne;
+
+    @Test
+    public void testAge() throws Exception {
+        personne = new Personne("Ismael","Cherif");
+        personne.setAge(1988);
+        assertTrue(2016-1988==personne.getAge());
+    }
+}
